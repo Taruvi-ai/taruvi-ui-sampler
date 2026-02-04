904250bfb1cfe3a2e2a6a9327e75398682412db1
avadecha <134299064+avadecha@users.noreply.github.com>
2026-02-04 14:52:33 -0500

    updates

---
+++ src/theme.ts
@@ -0,0 +1,67 @@
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+import gray from "@mui/material/colors/grey";
+
+const LightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+  components: {
+    MuiChip: {
+      styleOverrides: {
+        labelSmall: {
+          lineHeight: "18px",
+        },
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: {
+        "main.MuiBox-root": {
+          backgroundColor: gray[100],
+        },
+        body: {
+          backgroundColor: gray[100],
+        },
+      },
+    },
+    MuiTypography: {
+      defaultProps: {
+        variant: "body2",
+      },
+    },
+  },
+});
+
+const DarkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  components: {
+    MuiChip: {
+      styleOverrides: {
+        labelSmall: {
+          lineHeight: "18px",
+        },
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: {
+        "main.MuiBox-root": {
+          backgroundColor: "#121212",
+        },
+        body: {
+          backgroundColor: "#121212",
+        },
+      },
+    },
+    MuiTypography: {
+      defaultProps: {
+        variant: "body2",
+      },
+    },
+  },
+});
+
+const DarkThemeWithResponsiveFontSizes = responsiveFontSizes(DarkTheme);
+const LightThemeWithResponsiveFontSizes = responsiveFontSizes(LightTheme);
+
+export { LightThemeWithResponsiveFontSizes, DarkThemeWithResponsiveFontSizes };
